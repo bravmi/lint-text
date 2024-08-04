ad21d42e3835a6ca34dde7c8c4a7d9728d849146
Michael T <bravmech@gmail.com>
2024-08-04 16:33:54 +0200

    chore: Remove duplicate proselint command and improve echo statements in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/sh -l
 
-LINT_LOG=lint.log
-
 if [ -z $INPUT_TOOL ]; then
     echo "You must specify either 'write-good' or 'proselint' as the first argument."
     exit 1
 fi
 
 # Find all markdown files and run the selected tool on them
-FILES=$(find . -name "*.md" -not -path $INPUT_IGNORE_PATHS)
-$INPUT_TOOL $FILES >> $LINT_LOG
-cat $LINT_LOG
-cat ./README.md
+files=$(find . -name "*.md" -not -path $INPUT_IGNORE_PATHS)
+result=$($INPUT_TOOL $files)
+printf "result = $result\n"
+echo "result=$result" >> $GITHUB_OUTPUT
